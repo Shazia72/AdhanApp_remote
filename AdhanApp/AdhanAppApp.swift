b1ef6013918e115be1f61bae7df5cd47c3adea09
Shazia_Idrees <vohragee@gmail.com>
2023-11-10 09:18:10 -0500

    Initial Commit

---
+++ AdhanApp/AdhanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdhanAppApp.swift
+//  AdhanApp
+//
+//  Created by Shazia Vohra on 2023-11-10.
+//
+
+import SwiftUI
+
+@main
+struct AdhanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
